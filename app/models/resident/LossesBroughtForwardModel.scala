fba1a5b8c2b41f72572dd0f7229cda00ce76c637
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-03-10 11:21:47 +0000

    Updated headers to reflect the correct copyright year of 2017

--- app/models/resident/LossesBroughtForwardModel.scala
+++ app/models/resident/LossesBroughtForwardModel.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,4 +23,3 @@ case class LossesBroughtForwardModel(option : Boolean)
 object LossesBroughtForwardModel {
   implicit val format = Json.format[LossesBroughtForwardModel]
 }
-
--- test/controllers/ReportControllerSpec/FinalSummaryActionSpec.scala
+++ test/controllers/ReportControllerSpec/FinalSummaryActionSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -182,4 +182,3 @@ class FinalSummaryActionSpec extends UnitSpec with WithFakeApplication with Fake
     }
   }
 }
-
