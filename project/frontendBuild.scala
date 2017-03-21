337d46e606b0e892f4cd6973a31626e82545e020
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-03-21 10:12:50 +0000

    Updated bootstrap and play-ui dependencies

--- project/frontendBuild.scala
+++ project/frontendBuild.scala
@@ -30,14 +30,14 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.15.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.19.0",
     "uk.gov.hmrc" %% "play-partials" % "5.3.0",
     "uk.gov.hmrc" %% "play-authorised-frontend" % "6.3.0",
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.2.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
-    "uk.gov.hmrc" %% "play-ui" % "7.0.0",
+    "uk.gov.hmrc" %% "play-ui" % "7.2.0",
     "uk.gov.hmrc" %% "http-caching-client" % "6.2.0",
     "uk.gov.hmrc" %% "mongo-caching" % "4.0.0",
     "it.innove" 	%  "play2-pdf" % "1.5.1"
