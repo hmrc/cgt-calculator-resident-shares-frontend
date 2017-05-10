794c76c6ec1aca344a20a77b31b18d002a5097ad
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2017-05-10 15:33:33 +0100

    Added SaUserController

---
+++ app/controllers/SaUserController.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import common.Dates.requestFormatter
+import connectors.CalculatorConnector
+import constructors.CalculateRequestConstructor
+import constructors.resident.CalculateRequestConstructor
+import controllers.predicates.ValidActiveSession
+import forms.SaUserForm
+import forms.resident.SaUserForm
+import models.resident._
+import models.resident.shares.{DeductionGainAnswersModel, GainAnswersModel, YourAnswersSummaryModel}
+import play.api.mvc.{Action, AnyContent, Result}
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
+import play.api.data.Form
+import uk.gov.hmrc.play.http.HeaderCarrier
+import views.html.calculation.whatNext.saUser
+
+import scala.concurrent.Future
+
+object SaUserController extends SaUserController {
+  val calculatorConnector = CalculatorConnector
+}
+
+trait SaUserController extends ValidActiveSession {
+
+  val calculatorConnector: CalculatorConnector
+
+  val saUser: Action[AnyContent] = ValidateSession.async {
+    implicit request =>
+      Future.successful(Ok(views.html.calculation.whatNext.saUser(SaUserForm.saUserForm)))
+  }
+
+  val submitSaUser: Action[AnyContent] = ValidateSession.async { implicit request =>
+
+    def chargeableGain(grossGain: BigDecimal,
+                       deductionGainAnswers: DeductionGainAnswersModel,
+                       chargeableGainAnswers: GainAnswersModel,
+                       maxAEA: BigDecimal)(implicit hc: HeaderCarrier): Future[Option[ChargeableGainResultModel]] = {
+      if (grossGain > 0) calculatorConnector.calculateRttShareChargeableGain(chargeableGainAnswers, deductionGainAnswers, maxAEA)
+      else Future.successful(None)
+    }
+
+    def totalTaxableGain(chargeableGain: Option[ChargeableGainResultModel] = None,
+                         deductionGainAnswers: DeductionGainAnswersModel,
+                         chargeableGainAnswers: GainAnswersModel,
+                         incomeAnswersModel: IncomeAnswersModel,
+                         maxAEA: BigDecimal)(implicit hc: HeaderCarrier): Future[Option[TotalGainAndTaxOwedModel]] = {
+      if (chargeableGain.isDefined && chargeableGain.get.chargeableGain > 0 &&
+        incomeAnswersModel.personalAllowanceModel.isDefined && incomeAnswersModel.currentIncomeModel.isDefined) {
+        calculatorConnector.calculateRttShareTotalGainAndTax(chargeableGainAnswers, deductionGainAnswers, maxAEA, incomeAnswersModel)
+      }
+      else Future.successful(None)
+    }
+
+    def taxYearStringToInteger(taxYear: String): Future[Int] = {
+      Future.successful((taxYear.take(2) + taxYear.takeRight(2)).toInt)
+    }
+
+    def saAction(disposalValue: BigDecimal, taxOwed: Option[BigDecimal], maxAEA: BigDecimal): Future[Result] = {
+      taxOwed match {
+        case Some(tax) if tax > 0 => Future{Redirect(controllers.routes.WhatNextSAController.whatNextSAGain())}
+        case _ if disposalValue >= 4 * maxAEA => Future{Redirect(controllers.routes.WhatNextSAController.whatNextSAOverFourTimesAEA())}
+        case _ => Future{Redirect(controllers.routes.WhatNextSAController.whatNextSANoGain())}
+      }
+    }
+
+    def nonSaAction(taxOwed: Option[BigDecimal]) = {
+      taxOwed match {
+        case Some(gain) if gain > 0 => Future{Redirect(controllers.routes.WhatNextNonSaController.whatNextNonSaGain())}
+        case _ => Future{Redirect(controllers.routes.WhatNextNonSaController.whatNextNonSaLoss())}
+      }
+    }
+
+    def routeAction(saUserModel: SaUserModel, totalGainAndTaxOwedModel: Option[TotalGainAndTaxOwedModel],
+                    maxAEA: BigDecimal, disposalValue: BigDecimal): Future[Result] = {
+      val taxOwed = totalGainAndTaxOwedModel.map {_.taxOwed}
+      if (saUserModel.isInSa) saAction(disposalValue, taxOwed, maxAEA)
+      else nonSaAction(taxOwed)
+    }
+
+    def errorAction(form: Form[SaUserModel]) = {
+      Future.successful(BadRequest(views.html.calculation.resident.properties.whatNext.saUser(form)))
+    }
+
+    def successAction(model: SaUserModel) = {
+      for {
+        answers <- calculatorConnector.getShareGainAnswers
+        grossGain <- calculatorConnector.calculateRttShareGrossGain(answers)
+        deductionAnswers <- calculatorConnector.getShareDeductionAnswers
+        taxYear <- calculatorConnector.getTaxYear(answers.disposalDate.format(requestFormatter))
+        taxYearInt <- taxYearStringToInteger(taxYear.get.calculationTaxYear)
+        maxAEA <- calculatorConnector.getFullAEA(taxYearInt)
+        chargeableGain <- chargeableGain(grossGain, deductionAnswers, answers, maxAEA.get)
+        incomeAnswers <- calculatorConnector.getShareIncomeAnswers
+        finalResult <- totalTaxableGain(chargeableGain, deductionAnswers, answers, incomeAnswers, maxAEA.get)
+        route <- routeAction(model, finalResult, maxAEA.get, CalculateRequestConstructor.determineDisposalValueToUse(answers))
+      } yield route
+    }
+
+    SaUserForm.saUserForm.bindFromRequest().fold(errorAction, successAction)
+  }
+}
---
+++ test/controllers/SaUserControllerSpec.scala
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import assets.{MessageLookup, ModelsAsset}
+import connectors.CalculatorConnector
+import controllers.helpers.FakeRequestHelper
+import models.resident.{ChargeableGainResultModel, TotalGainAndTaxOwedModel}
+import models.resident.shares.GainAnswersModel
+import models.resident.shares.DeductionGainAnswersModel
+import org.jsoup.Jsoup
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.OneAppPerSuite
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.UnitSpec
+
+import scala.concurrent.Future
+
+class SaUserControllerSpec extends UnitSpec with OneAppPerSuite with FakeRequestHelper with MockitoSugar {
+
+  def setupController(gainAnswersModel: GainAnswersModel, chargeableGain: BigDecimal, totalGain: BigDecimal,
+                      taxOwed: BigDecimal): SaUserController = {
+    val mockConnector = mock[CalculatorConnector]
+
+    when(mockConnector.getShareGainAnswers(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(gainAnswersModel))
+
+    when(mockConnector.getShareDeductionAnswers(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(ModelsAsset.deductionAnswersLeastPossibles))
+
+    when(mockConnector.getShareIncomeAnswers(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(ModelsAsset.incomeAnswers))
+
+    when(mockConnector.calculateRttShareGrossGain(ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(totalGain))
+
+    when(mockConnector.calculateRttShareChargeableGain(ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(Some(ChargeableGainResultModel(totalGain, chargeableGain, 0, 0, 0, 0, 0, None, None, 0, 0))))
+
+    when(mockConnector.calculateRttShareTotalGainAndTax(ArgumentMatchers.any(), ArgumentMatchers.any(),
+      ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(Some(TotalGainAndTaxOwedModel(totalGain, chargeableGain, 11000, 0, 5000, 10000, 5, None, None, None, None, 0, 0))))
+
+    when(mockConnector.getFullAEA(ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(Some(BigDecimal(11000))))
+
+    when(mockConnector.getTaxYear(ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(Some(ModelsAsset.taxYearModel)))
+
+    new SaUserController {
+      override val calculatorConnector: CalculatorConnector = mockConnector
+    }
+  }
+
+  "Calling .saUser" when {
+
+    "no session is provided" should {
+      lazy val result = SaUserController.saUser(fakeRequest)
+
+      "return a status of 303" in {
+        status(result) shouldBe 303
+      }
+
+      "redirect to the missing session page" in {
+        redirectLocation(result).get should include("/calculate-your-capital-gains/resident/properties/session-timeout")
+      }
+    }
+
+    "a session is provided" should {
+      lazy val result = SaUserController.saUser(fakeRequestWithSession)
+
+      "return a status of 200" in {
+        status(result) shouldBe 200
+      }
+
+      "load the saUser page" in {
+        Jsoup.parse(bodyOf(result)).title() shouldBe MessageLookup.SaUser.title
+      }
+    }
+  }
+
+  "Calling .submitSaUser" when {
+
+    "no session is provided" should {
+      lazy val result = SaUserController.submitSaUser(fakeRequest)
+
+      "return a status of 303" in {
+        status(result) shouldBe 303
+      }
+
+      "redirect to the missing session page" in {
+        redirectLocation(result).get should include("/calculate-your-capital-gains/resident/properties/session-timeout")
+      }
+    }
+
+//    "a non-sa user is submitted" when {
+//      val form = "isInSa" -> "No"
+//
+//      "there is no tax liability" should {
+//        lazy val controller = setupController(ModelsAsset.gainAnswersMostPossibles, 0, -10000, 0)
+//        lazy val result = controller.submitSaUser(fakeRequestToPOSTWithSession(form))
+//
+//        "return a status of 303" in {
+//          status(result) shouldBe 303
+//        }
+//
+//        "redirect to the nonSa loss what next page" in {
+//          redirectLocation(result) shouldBe Some(controllers.routes.WhatNextNonSaController.whatNextNonSaLoss().url)
+//        }
+//      }
+//
+//      "there is a tax liability" should {
+//        lazy val controller = setupController(ModelsAsset.gainAnswersMostPossibles, 10000, 5000, 2000)
+//        lazy val result = controller.submitSaUser(fakeRequestToPOSTWithSession(form))
+//
+//        "return a status of 303" in {
+//          status(result) shouldBe 303
+//        }
+//
+//        "redirect to the nonSa gain what next page" in {
+//          redirectLocation(result) shouldBe Some(controllers.routes.WhatNextNonSaController.whatNextNonSaGain().url)
+//        }
+//      }
+//    }
+//
+//    "a sa user is submitted" when {
+//      val form = "isInSa" -> "Yes"
+//
+//      "there is a tax liability" should {
+//        lazy val controller = setupController(ModelsAsset.gainAnswersMostPossibles, 10000, 5000, 2000)
+//        lazy val result = controller.submitSaUser(fakeRequestToPOSTWithSession(form))
+//
+//        "return a status of 303" in {
+//          status(result) shouldBe 303
+//        }
+//
+//        "redirect to the nonSa gain what next page" in {
+//          redirectLocation(result) shouldBe Some(controllers.routes.WhatNextSAController.whatNextSAGain().url)
+//        }
+//      }
+//
+//      "there is no tax liability and a disposal value less than 4*AEA" should {
+//        lazy val controller = setupController(ModelsAsset.gainAnswersMostPossibles, 0, -10000, 0)
+//        lazy val result = controller.submitSaUser(fakeRequestToPOSTWithSession(form))
+//
+//        "return a status of 303" in {
+//          status(result) shouldBe 303
+//        }
+//
+//        "redirect to the nonSa loss what next page" in {
+//          redirectLocation(result) shouldBe Some(controllers.routes.WhatNextSAController.whatNextSANoGain().url)
+//        }
+//      }
+//
+//      "there is no tax liability and a disposal value greater than 4*AEA" should {
+//        lazy val controller = setupController(ModelsAsset.gainLargeDisposalValue, 0, -10000, 0)
+//        lazy val result = controller.submitSaUser(fakeRequestToPOSTWithSession(form))
+//
+//        "return a status of 303" in {
+//          status(result) shouldBe 303
+//        }
+//
+//        "redirect to the nonSa loss with value greater than what next page" in {
+//          redirectLocation(result) shouldBe Some(controllers.routes.WhatNextSAController.whatNextSAOverFourTimesAEA().url)
+//        }
+//      }
+//    }
+  }
+}
