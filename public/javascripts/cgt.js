a6d2814ccedb9b66bc3d621cf731ddde64f1060d
Kieran Puaar <Kieran.Puaar@digital.hmrc.gov.uk>
2018-05-16 08:54:13 +0100

    commentig js script preventing ur banner

--- public/javascripts/cgt.js
+++ public/javascripts/cgt.js
@@ -43,11 +43,12 @@ $(document).ready($(function () {
     }
   });
 
-  $('[data-metrics]').each(function () {
-    var metrics = $(this).attr('data-metrics');
-    var parts = metrics.split(':');
-    ga('send', 'event', parts[0], parts[1], parts[2]);
-  });
+// TODO: investigate why this js is currently not working and if required - links to UR Banner implementation
+//  $('[data-metrics]').each(function () {
+//    var metrics = $(this).attr('data-metrics');
+//    var parts = metrics.split(':');
+//    ga('send', 'event', parts[0], parts[1], parts[2]);
+//  });
 
     var reportLink = $('#get-help-action');
     var reportLocation = window.location.pathname;
