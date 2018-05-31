a00624daa378f621519a2d71690e641161639498
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2018-05-31 11:37:30 +0100

    Fixed summary page welsh inconsistencies.

--- app/common/Dates.scala
+++ app/common/Dates.scala
@@ -20,6 +20,8 @@ import java.time._
 import java.time.format.{DateTimeFormatter, ResolverStyle}
 import java.time.temporal.ChronoUnit
 
+import play.api.i18n.Messages
+
 import scala.concurrent.Future
 
 object Dates {
@@ -60,6 +62,21 @@ object Dates {
     }
     else {
       Future.successful(taxYearToString(year))
+    }
+  }
+
+  object TemplateImplicits {
+    implicit class RichDate(date: LocalDate) {
+      def localFormat(pattern: String)(implicit lang: play.api.i18n.Lang, messages: Messages): String = {
+        if(lang.language == "cy") {
+          val monthNum = date.getMonthValue
+          val welshFormatter = DateTimeFormatter.ofPattern(s"""d '${messages(s"calc.month.$monthNum")}' YYYY""")
+          date.format(welshFormatter)
+        } else {
+          val localFormatter = DateTimeFormatter.ofPattern(pattern, lang.toLocale)
+          date.format(localFormatter)
+        }
+      }
     }
   }
 }
