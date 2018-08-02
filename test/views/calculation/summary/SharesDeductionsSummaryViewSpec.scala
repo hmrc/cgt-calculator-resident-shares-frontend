8d988f60dfbfb922f4d4bd77d9dc2a3be13eccd3
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2018-08-02 12:51:01 +0100

    HTML5 validation fix. Tests removed.

--- test/views/calculation/summary/SharesDeductionsSummaryViewSpec.scala
+++ test/views/calculation/summary/SharesDeductionsSummaryViewSpec.scala
@@ -143,10 +143,6 @@ class SharesDeductionsSummaryViewSpec extends UnitSpec with WithFakeApplication 
 
           lazy val link = savePDFSection.select("a")
 
-          "has the type submit" in {
-            link.attr("type").equals("submit") shouldBe true
-          }
-
           "has the class bold-small" in {
             link.hasClass("bold-small") shouldBe true
           }
