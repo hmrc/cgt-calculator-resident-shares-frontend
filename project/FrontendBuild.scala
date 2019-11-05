51e646ccc2261d076c35505bf16ffdd21b290e70
stephen-oppong-beduh <stephen.oppong.beduh@digital.hmrc.gov.uk>
2019-11-05 12:25:56 +0000

    DL-1404 Dep Update

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -39,7 +39,7 @@ private object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-26" % "0.39.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.34.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "7.39.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "8.3.0-play-26",
     "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "8.3.0",
     "it.innove" % "play2-pdf" % "1.5.2",
@@ -58,9 +58,9 @@ private object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % "3.8.0-play-26" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-26" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % scope,
-        "org.mockito" % "mockito-core" % "2.27.0" % scope,
+        "org.mockito" % "mockito-core" % "3.1.0" % scope,
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.jsoup" % "jsoup" % "1.11.3" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
